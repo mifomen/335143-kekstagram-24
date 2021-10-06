264c74a573952a4f2a9b2f5d828bec8f9da7e1a1
Ilya <mifomen@yandex.ru>
2021-10-06 21:23:53 +0300

    fix for eslint x3

--- js/main.js
+++ js/main.js
@@ -16,21 +16,30 @@ checkedMaxLength(testString, 19);
 
 const randomNames = ['Полина','Иван','Мира','Виктория','Ариана','Артём','Виктор','Мирослава','Даниил','Екатерина','Михаил','Анна','Татьяна','Адам','Никита','Роман','Стефания','Дмитрий','Захар','Валентина','Алина','Ольга','Вероника','Мартин','Артемий','София','Василий','Антон','Егор','Матвей','Диана','Антонина','Ярослава','Александр','Дамир','Тимур','Дарья','Владимир','Леонид','Алёна','Агата','Милана','Руслан','Андрей','Олег','Арсен','Николай','Валерия','Ярослав','Владислав'];
 
-const comments = [
+const messages = [
   'Всё отлично!',  'В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный моме'];
 
 const descriptions = [
   'На кладбище идет, что ли?','Красивое','Мы поедем, мы помчимся','Да здравствуют вредные привычки!','Шрек уже не тот','Что, черт возьми, происходит в австралийской глубинке?','Let\'s rock','Руководитель отдела ИТ-разработки прячется от подчиненных','Всё свое ношу с собой','Раздуло'];
 
+const getRandomMessages = (elements) => {
+  if (getRandom(0,1)===1) {
+    return getRandomArrayElement(elements) + getRandomArrayElement(elements);
+  }
+  return getRandomArrayElement(elements);
+};
 
 const SIMILAR_PHOTOS_COUNT = 25;
 
-const getRandomArrayElement = function(elements) {
+function getRandomArrayElement(elements) {
   return elements[getRandom(0, elements.length - 1)];
-};
+}
 
 const getAvatarImage = function() {
-  return 'img/avatar-' + [getRandom(1, 6)] + '.svg';
+  const firstUrlPart = 'img/avatar-';
+  const secondUrlPart = '.svg';
+  const imageId = [getRandom(1, 6)];
+  return ''.concat(firstUrlPart,imageId,secondUrlPart);
 };
 
 let indexId = 0;
@@ -40,11 +49,17 @@ const createPostPhoto = function() {
     url: getAvatarImage(),
     description: getRandomArrayElement(descriptions),
     likes: getRandom(15,200),
-    comments: getRandomArrayElement(comments),
+    // message: getRandomArrayElement(messages),
+    message: getRandomMessages(messages),
     name: getRandomArrayElement(randomNames),
   };
 };
 
 
-const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createPostPhoto);
+const getRandomPosts = function (countOfPhotos) {
+  return Array.from({length: countOfPhotos}, createPostPhoto);
+};
+
+const similarPhotos = getRandomPosts(SIMILAR_PHOTOS_COUNT);
+// const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createPostPhoto);
 
