baf1a74dd4db045a45149235135a3c3e25dfcf3b
Ilya <mifomen@yandex.ru>
2021-10-08 18:21:33 +0300

    init modules

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 import {checkStringLength} from './utils/check-string-length.js';
 import {getRandomPositiveInteger} from './utils/get-random-positive-integer.js';
 
-// eslint-disable-next-line
-console.log(getRandomPositiveInteger(123,1233))
-
 const RANDOM_NAMES = ['Полина','Иван','Мира','Виктория','Ариана','Артём','Виктор','Мирослава','Даниил','Екатерина','Михаил','Анна','Татьяна','Адам','Никита','Роман','Стефания','Дмитрий','Захар','Валентина','Алина','Ольга','Вероника','Мартин','Артемий','София','Василий','Антон','Егор','Матвей','Диана','Антонина','Ярослава','Александр','Дамир','Тимур','Дарья','Владимир','Леонид','Алёна','Агата','Милана','Руслан','Андрей','Олег','Арсен','Николай','Валерия','Ярослав','Владислав'];
 const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный моме'];
 const DESCRIPTIONS = ['На кладбище идет, что ли?','Красивое','Мы поедем, мы помчимся','Да здравствуют вредные привычки!','Шрек уже не тот','Что, черт возьми, происходит в австралийской глубинке?','Let\'s rock','Руководитель отдела ИТ-разработки прячется от подчиненных','Всё свое ношу с собой','Раздуло'];
@@ -11,40 +8,29 @@ const DESCRIPTIONS = ['На кладбище идет, что ли?','Краси
 const SIMILAR_PHOTOS_COUNT = 25;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
+const TEST_STRING = 'Ivanov Ivan Ivanich';
 
-const getRandom = (min,max) => {
-  if ( max < min || max < 0 || min < 0 ) {
-    return false;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+checkStringLength(TEST_STRING, 19);
 
 function getRandomArrayElement(elements) {
-  return elements[getRandom(0, elements.length - 1)];
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
 }
 
 const getRandomMessages = (elements) => {
-  if (getRandom(0,1)===1) {
+  if (getRandomPositiveInteger(0,1)===1) {
     return `${getRandomArrayElement(elements)} ${getRandomArrayElement(elements)}`;
   }
   return getRandomArrayElement(elements);
 };
 
-function checkedMaxLength (string,max) {
-  return string.length <= max;
-}
-
-const testString = 'Ivanov Ivan Ivanich';
-checkedMaxLength(testString, 19);
-checkStringLength(testString, 19);
 
 function getAvatarImage() {
-  return `img/avatar-${getRandom(1, 6)}.svg`;
+  return `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`;
 }
 
-function getRandomComments() {
+function getRandomPositiveIntegerComments() {
   return {
-    id: getRandom(1,800),
+    id: getRandomPositiveInteger(1,800),
     avatar: getAvatarImage(),
     message: getRandomMessages(COMMENTS),
     name: getRandomArrayElement(RANDOM_NAMES),
@@ -57,12 +43,11 @@ function createPostPhoto() {
     id: postId++,
     url: getAvatarImage(),
     description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandom(MIN_LIKES,MAX_LIKES),
-    comments: getRandomComments(COMMENTS),
+    likes: getRandomPositiveInteger(MIN_LIKES,MAX_LIKES),
+    comments: getRandomPositiveIntegerComments(COMMENTS),
     name: getRandomArrayElement(RANDOM_NAMES),
   };
 }
-
 
 const getRandomPosts = function(countOfPhotos) {
   return Array.from({length: countOfPhotos}, createPostPhoto);
@@ -70,7 +55,6 @@ const getRandomPosts = function(countOfPhotos) {
 
 // eslint-disable-next-line
 const similarPhotos = getRandomPosts(SIMILAR_PHOTOS_COUNT);
-// const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createPostPhoto);
 
 // eslint-disable-next-line
 console.log(similarPhotos)
