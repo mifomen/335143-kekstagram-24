2e90434aa7350da73edb4ce82d7c5ab993500f38
Ilya Koshelev <mifomen@yandex.ru>
2021-10-16 19:04:00 +0300

    fix after review

--- js/main.js
+++ js/main.js
@@ -1,33 +1,26 @@
-// eslint-disable-next-line
 import {similarPhotos} from './data/gen-new-photo-posts.js';
 
 // eslint-disable-next-line
-// import {renderPicture} from './render/render-picture.js';
-
-
-// import {similarPhotos} from './data/gen-new-photo-posts.js';
+import {renderPicture} from './render/render-picture.js';
 
 // eslint-disable-next-line
 console.log(similarPhotos)
-// export const renderPicture = function ()  {
 
-// };
 
-let picturesList = document.querySelector('.pictures');
-let pictureTemplate = document.querySelector('#picture');
-let pictireFragment = document.createDocumentFragment();
-console.log(pictureTemplate)
+// const picturesList = document.querySelector('.pictures');
+// const pictureTemplate = document.querySelector('#picture');
+// const pictureFragment = document.createDocumentFragment();
 
-similarPhotos.forEach((photoPost) => {
-  const testPic = pictureTemplate.content.cloneNode(true)
-  testPic.querySelector('.picture__img').src = photoPost.url;
-  testPic.querySelector('.picture__comments').textContent = photoPost.comments;
-  testPic.querySelector('.picture__likes').textContent = photoPost.likes;
-console.log(photoPost.url);
-  picturesList.appendChild(testPic);
-  // testPic.docu photoPost.url =
-  console.log(testPic)
-})
+// similarPhotos.forEach((photoPost) => {
+// const photoElement = pictureTemplate.content.cloneNode(true);
+// photoElement.querySelector('.picture__img').src = photoPost.url;
+// photoElement.querySelector('.picture__comments').textContent = photoPost.length;
+// photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
+// pictureFragment.appendChild(photoElement);
+
+// });
+// picturesList.appendChild(pictureFragment)
+
 
 // task 7.2 module7-task2
 // let BigPicture = document.querySelector('.big-picture')
--- js/render/render-picture.js
+++ js/render/render-picture.js
@@ -1,9 +1,19 @@
 import {similarPhotos} from '../data/gen-new-photo-posts.js';
 
-// eslint-disable-next-line
-console.log(similarPhotos)
 export const renderPicture = function ()  {
+  const picturesList = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture');
+  const pictureFragment = document.createDocumentFragment();
 
+  similarPhotos.forEach((photoPost) => {
+    const photoElement = pictureTemplate.content.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = photoPost.url;
+    photoElement.querySelector('.picture__comments').textContent = photoPost.length;
+    photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
+    pictureFragment.appendChild(photoElement);
+
+  });
+  picturesList.appendChild(pictureFragment);
 };
 
-
+renderPicture();
