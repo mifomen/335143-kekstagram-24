abe5d5e2c7328066b0e0cb1e7f576fc77043f4ed
Ilya Koshelev <mifomen@yandex.ru>
2021-10-16 19:05:09 +0300

    fix //

--- js/main.js
+++ js/main.js
@@ -5,34 +5,3 @@ import {renderPicture} from './render/render-picture.js';
 
 // eslint-disable-next-line
 console.log(similarPhotos)
-
-
-// const picturesList = document.querySelector('.pictures');
-// const pictureTemplate = document.querySelector('#picture');
-// const pictureFragment = document.createDocumentFragment();
-
-// similarPhotos.forEach((photoPost) => {
-// const photoElement = pictureTemplate.content.cloneNode(true);
-// photoElement.querySelector('.picture__img').src = photoPost.url;
-// photoElement.querySelector('.picture__comments').textContent = photoPost.length;
-// photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
-// pictureFragment.appendChild(photoElement);
-
-// });
-// picturesList.appendChild(pictureFragment)
-
-
-// task 7.2 module7-task2
-// let BigPicture = document.querySelector('.big-picture')
-
-// BigPicture.classList.remove('hidden')
-
-// // import {getRandomArrayElement} from './utils/get-random-array-element.js'
-// import {getRandomPositiveInteger} from './utils/get-random-positive-integer.js';
-
-
-// document.querySelector('.big-picture__img').src=similarPhotos[getRandomPositiveInteger(0,similarPhotos.length)].url;
-// // document.querySelector('.big-picture__img').src="mifomen.jps"
-// document.querySelector('.likes-count').textContent=similarPhotos[getRandomPositiveInteger(0,similarPhotos.length)].likes;
-// console.log(similarPhotos[getRandomPositiveInteger(0,similarPhotos.length)].likes)
-// document.querySelector('.comments-count').textContent=similarPhotos[getRandomPositiveInteger(0,similarPhotos.length)].comments;
