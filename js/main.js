650d689c8ec15636fb1c9318d45fb23f3da3c707
Ilya Koshelev <mifomen@yandex.ru>
2021-10-16 10:30:29 +0300

    on test

--- js/main.js
+++ js/main.js
@@ -2,4 +2,29 @@
 import {similarPhotos} from './data/gen-new-photo-posts.js';
 
 // eslint-disable-next-line
-import {renderPicture} from './render/render-picture.js';
+// import {renderPicture} from './render/render-picture.js';
+
+
+// import {similarPhotos} from './data/gen-new-photo-posts.js';
+
+// eslint-disable-next-line
+console.log(similarPhotos)
+// export const renderPicture = function ()  {
+
+// };
+
+let picturesList = document.querySelector('.pictures');
+let pictureTemplate = document.querySelector('#picture');
+let pictireFragment = document.createDocumentFragment();
+console.log(pictureTemplate)
+
+similarPhotos.forEach((photoPost) => {
+  const testPic = pictureTemplate.content.cloneNode(true)
+  testPic.querySelector('.picture__img').src = photoPost.url;
+  testPic.querySelector('.picture__comments').textContent = photoPost.comments;
+  testPic.querySelector('.picture__likes').textContent = photoPost.likes;
+console.log(photoPost.url);
+  picturesList.appendChild(testPic);
+  // testPic.docu photoPost.url =
+  console.log(testPic)
+})
