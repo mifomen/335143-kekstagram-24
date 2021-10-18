38fceb382c4c9185454a2dbfc80f2c84ad26180f
Ilya Koshelev <mifomen@yandex.ru>
2021-10-18 07:14:10 +0300

    fix

--- js/render/render-picture.js
+++ js/render/render-picture.js
@@ -1,13 +1,14 @@
 import {similarPhotos} from '../data/gen-new-photo-posts.js';
+import {getPhotoById} from '../utils/get-photo-by-id.js';
 
 export const renderPicture = function ()  {
   const picturesList = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture');
   const pictureFragment = document.createDocumentFragment();
 
-  similarPhotos.forEach((photoPost) => {
+  similarPhotos.forEach((photoPost,index) => {
     const photoElement = pictureTemplate.content.cloneNode(true);
-    photoElement.querySelector('.picture__img').src = photoPost.url;
+    photoElement.querySelector('.picture__img').src = getPhotoById(index+1);
     photoElement.querySelector('.picture__comments').textContent = photoPost.length;
     photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
     pictureFragment.appendChild(photoElement);
