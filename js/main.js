9060daab95e5d28f25e9ef623d11e1a93c33384b
Ilya Koshelev <mifomen@yandex.ru>
2021-10-19 15:01:45 +0300

    fix main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,4 @@ import {similarPhotos} from './data/gen-new-photo-posts.js';
 // eslint-disable-next-line
 import {renderPicture} from './render/render-picture.js';
 // eslint-disable-next-line
-console.log(similarPhotos)
-// eslint-disable-next-line
 import {renderRandomComments} from './render/render-random-coments.js';
