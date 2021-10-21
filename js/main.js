d5835b85940fb4fef48d67b78ea34d9c0d0a47f9
Ilya <mifomen@yandex.ru>
2021-10-21 22:12:09 +0300

    task8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line
-import {similarPhotos} from './data/gen-new-photo-posts.js';
-// eslint-disable-next-line
-import {renderPicture} from './render/render-picture.js';
-// eslint-disable-next-line
-import {renderRandomComments} from './render/render-random-coments.js';
+import './data/gen-new-photo-posts.js';
+import './render/render-picture.js';
+import './render/render-random-coments.js';
