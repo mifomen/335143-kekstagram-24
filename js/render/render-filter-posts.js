26a432d0bfe920b1aa78c2f70546385c9872c0e1
Ilya <mifomen@yandex.ru>
2021-11-17 21:42:01 +0300

    fix

--- js/render/render-filter-posts.js
+++ js/render/render-filter-posts.js
@@ -24,7 +24,9 @@ const sortByPopular = (array) => array.slice().sort(comparePosts);
 const sortMinPictures = (array) => {
   minPicturesFilter.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('img-filters__button') && !evt.target.classList.contains('img-filters__button--active')) {
-      document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+      const activeBtn = document.querySelector('.img-filters__button--active');
+      activeBtn.classList.remove('img-filters__button--active');
+
       evt.target.classList.add('img-filters__button--active');
       const filterName = evt.target.id;
       debounce(() => {
