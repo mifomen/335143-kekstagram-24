4f52f5bb1f7b76184b37a7b404d48899d44a41b2
Ilya Koshelev <mifomen@yandex.ru>
2021-11-20 17:30:29 +0300

    change after review 1 part

--- js/render/update-comments.js
+++ js/render/update-comments.js
@@ -4,19 +4,19 @@ const COMMENTS_STEP = 5;
 
 const updateComments = (dataArray) => {
 
-  const bigPicture = document.querySelector('.big-picture');
+  const bigPictureElement = document.querySelector('.big-picture');
   const allPosts = document.querySelectorAll('.picture__img');
-  bigPicture.classList.add('hidden');
-  bigPicture.classList.remove('overlay');
+  bigPictureElement.classList.add('hidden');
+  bigPictureElement.classList.remove('overlay');
   document.body.classList.remove('modal-open');
 
-  for (const post of allPosts) {
+  for (const postElement of allPostsElements) {
     post.addEventListener('click', (evt) => {
 
       evt.preventDefault();
-      bigPicture.querySelector('.big-picture__img img').src = evt.target.src;
-      bigPicture.classList.remove('hidden');
-      bigPicture.classList.toggle('overlay');
+      bigPictureElement.querySelector('.big-picture__img img').src = evt.target.src;
+      bigPictureElement.classList.remove('hidden');
+      bigPictureElement.classList.toggle('overlay');
       document.body.classList.toggle('modal-open');
       document.addEventListener('keydown', onEscapePress);
       let renderCommentsCount = COMMENTS_STEP;
@@ -29,33 +29,33 @@ const updateComments = (dataArray) => {
       });
 
       createComments(obj,renderCommentsCount);
-      const commentsLoadBtn = document.querySelector('.social__comment-count');
+      const commentsLoadBtnElementElement = document.querySelector('.social__comment-count');
       const loadCommentsBtn = document.querySelector('.comments-loader');
 
       loadCommentsBtn.addEventListener('click', () => {
         renderCommentsCount+=COMMENTS_STEP;
 
         if (obj.comments.length - renderCommentsCount < COMMENTS_STEP) {
-          commentsLoadBtn.textContent=`${obj.comments.length} из ${obj.comments.length}`;
+          commentsLoadBtnElement.textContent=`${obj.comments.length} из ${obj.comments.length}`;
           createComments(obj,obj.comments.length);
           loadCommentsBtn.classList.add('hidden');
           return;
         }
         if (renderCommentsCount <= obj.comments.length) {
-          commentsLoadBtn.textContent=`${renderCommentsCount} из ${obj.comments.length}`;
+          commentsLoadBtnElement.textContent=`${renderCommentsCount} из ${obj.comments.length}`;
           createComments(obj,renderCommentsCount);
           loadCommentsBtn.classList.remove('hidden');
         } else {
           loadCommentsBtn.classList.add('hidden');
-          commentsLoadBtn.textContent=`${obj.comments.length} из ${obj.comments.length}`;
+          commentsLoadBtnElement.textContent=`${obj.comments.length} из ${obj.comments.length}`;
         }
       });
 
       if (renderCommentsCount <= obj.comments.length ) {
-        commentsLoadBtn.textContent=`${renderCommentsCount} из ${obj.comments.length}`;
+        commentsLoadBtnElement.textContent=`${renderCommentsCount} из ${obj.comments.length}`;
         loadCommentsBtn.classList.remove('hidden');
       } else {
-        commentsLoadBtn.textContent=`${obj.comments.length} из ${obj.comments.length}`;
+        commentsLoadBtnElement.textContent=`${obj.comments.length} из ${obj.comments.length}`;
         loadCommentsBtn.classList.add('hidden');
       }
     });
